0e23c3b8041f85d27acf6c3656f721021599ca0e
Michael Bassous <mbassous@gmail.com>
2020-01-18 17:11:02 -0800

    First commit

--- homease/main/models.py
+++ homease/main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from djangotoolbox.fields import ListField
 
-# Create your models here.
+class House(models.Model):
+    name = models.CharField(max_length=50)
+	address = models.CharField(max_length=50)
+
+class User(models.Model):
+	firstname = models.CharField(max_length=50)
+	lastname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+	phone = models.CharField(max_length=50)
+    venmo = models.CharField(max_length=50)
+	email = models.CharField(max_length=50)
+    house = models.ForeignKey(House, on_delete=models.CASCADE)
+
+class Bill(model.Model):
+    utility = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.IntegerField()
+    utilityusername = models.CharField(max_length=50)
+    utilitypassword = models.CharField(max_length=50)
+
+class Payment(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE)
+    bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
+    house = models.ForeignKey(House, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
+
+class Records(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE)
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=20, decimal_places=2)
+
